d152df472a2128f097336322c5c5cc9566e925f3
pschaefferkoetter <prschaefferkoetter@gmail.com>
2022-07-26 10:22:00 -0600

    add email validation test

--- microservice1/src/routes/__tests__/signup.test.js
+++ microservice1/src/routes/__tests__/signup.test.js
@@ -2,13 +2,20 @@
 const request = require('supertest');
 const app = require('../../app');
 
-it('should return an error for get request', () => {
-
-});
 
 it('should return an error for bad post request', async () => {
   await request(app)
     .post('/api/microservice1/signup')
-    .send({someData: 'data'})
+    .send({badData: 'bad data'})
+    .expect(409);
+
+  await request(app)
+    .post('/api/microservice1/signup')
+    .send({})
+    .expect(409);
+
+  await request(app)
+    .post('/api/microservice1/signup')
+    .send({email: 'an Invalid email'})
     .expect(409);
 });
--- microservice1/src/routes/signup.js
+++ microservice1/src/routes/signup.js
@@ -1,12 +1,19 @@
 const express = require('express');
+const { body, validationResult } = require('express-validator');
 
 const signUpRouter = express.Router();
 
-signUpRouter.post('/api/microservice1/signup', (req, res) => {
-  if (!req.body.email) {
-    console.log('here');
-    res.status(409).send({error: 'invalid!'});
-  }
+signUpRouter.post('/api/microservice1/signup',
+  body('username').isEmail().withMessage('Invalid Email!'),
+  (req, res) => {
+    const errors = validationResult(req);
+    const containsErrros = !errors.isEmpty();
+
+    if (containsErrros) {
+      res.status(409).send({error: 'invalid!'});
+    }
+
+    res.sendStatus(200);
 });
 
 module.exports = signUpRouter;
