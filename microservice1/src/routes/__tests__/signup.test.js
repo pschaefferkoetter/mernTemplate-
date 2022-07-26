67ebbb3a58ce147d9b33053041d812e01749938e
pschaefferkoetter <prschaefferkoetter@gmail.com>
2022-07-26 12:29:53 -0600

    feat(signup) add password constraints and tests

--- microservice1/src/routes/__tests__/signup.test.js
+++ microservice1/src/routes/__tests__/signup.test.js
@@ -2,20 +2,85 @@
 const request = require('supertest');
 const app = require('../../app');
 
+// check emails using express validator package
+describe('test email validity', () => {
+  let password = '';
+  let url = ''
 
-it('should return an error for bad post request', async () => {
-  await request(app)
-    .post('/api/microservice1/signup')
-    .send({badData: 'bad data'})
-    .expect(409);
+  beforeAll(() => {
+    password = 'A1bcdefg';
+    url = '/api/microservice1/signup';
+  })
 
-  await request(app)
-    .post('/api/microservice1/signup')
-    .send({})
-    .expect(409);
+  it('should return an error for bad post request', async () => {
+    await request(app)
+      .post(url)
+      .send({ password })
+      .expect(409);
 
-  await request(app)
-    .post('/api/microservice1/signup')
-    .send({email: 'an Invalid email'})
-    .expect(409);
+    await request(app)
+      .post(url)
+      .send({ email: 'an Invalid email', password })
+      .expect(409);
+  });
+
+  it('should return a code of 200 with proper email', async () => {
+    await request(app)
+     .post(url)
+     .send({ email: 'test@gamil.com', password })
+     .expect(200);
+  })
 });
+
+
+//test password:
+//  min length of alpanumeric chars
+//  min of 1 Upper Case
+//  min of 1 lower case
+//  min of 1 number
+
+describe('test password requirements', () =>{
+  let email = 'test@gmail.com';
+  let url = ''
+
+  beforeAll(() => {
+    email = 'test@gmail.com';
+    url = '/api/microservice1/signup';
+  });
+
+  it('should return 409 if password is less than 8 chars', async () => {
+    await request(app)
+      .post(url)
+      .send({ email, password: '1Abcd' })
+      .expect(409);
+  });
+
+  it('should return 409 if password does not contain at least 1 upper case letter', async () => {
+    await request(app)
+      .post(url)
+      .send({ email, password: '1abcdefg' })
+      .expect(409);
+
+  });
+
+  it('should return 409 if password does not contain at least 1 lower case letter', async () => {
+    await request(app)
+      .post(url)
+      .send({ email, password: '1ABCDEFG'})
+      .expect(409);
+  });
+
+  it('should return 409 if password does not inlcude at least one number',async () => {
+    await request(app)
+      .post(url)
+      .send({ email, password: 'ABcdEFGH'})
+      .expect(409);
+  });
+
+  it('should return 200 if password is valid ', async () => {
+    await request(app)
+      .post(url)
+      .send({ email, password: '1Abcdefg '})
+      .expect(200);
+  })
+})
--- microservice1/src/routes/signup.js
+++ microservice1/src/routes/signup.js
@@ -4,7 +4,13 @@ const { body, validationResult } = require('express-validator');
 const signUpRouter = express.Router();
 
 signUpRouter.post('/api/microservice1/signup',
-  body('username').isEmail().withMessage('Invalid Email!'),
+  [
+    body('email').isEmail().withMessage('Invalid Email!'),
+    body('password').isLength({min: 8}).withMessage('Password must be at least 8 characters'),
+    body('password').matches(/^(.*[a-z].*)$/).withMessage('Passord must constain at least 1 lowercase letter'),
+    body('password').matches(/^(.*[A-Z].*)$/).withMessage('Passord must constain at least 1 uppercase letter'),
+    body('password').matches(/^(.*\d.*)$/).withMessage('Password must have at least 1 number')
+  ],
   (req, res) => {
     const errors = validationResult(req);
     const containsErrros = !errors.isEmpty();
